123cb357b6e4a01d436c1da311d6b6cc54ffbfad
Nyame100 <nyamerichmond100@gmail.com>
2023-10-27 12:41:58 +0200

    menu-bar update

--- src/utils/mobile-nav.js
+++ src/utils/mobile-nav.js
@@ -11,9 +11,15 @@ const mobileNav = () => {
     isMobileNavOpen = !isMobileNavOpen;
     if (isMobileNavOpen) {
       mobileNav.style.display = "flex";
+      // added //
+      headerBtn.classList.add("open");
+      //
       document.body.style.overflowY = "hidden";
     } else {
       mobileNav.style.display = "none";
+      // added  //
+      headerBtn.classList.remove("open");
+      //
       document.body.style.overflowY = "auto";
     }
   });
